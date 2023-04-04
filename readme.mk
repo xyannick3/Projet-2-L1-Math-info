938c21d063c41e8f931ab5b654fae521ba7ad1bc
xyannick3 <xyannick3@gmail.com>
2023-04-04 16:09:49 +0200

    addition wallisyou.py

--- readme.mk
+++ readme.mk
@@ -1,11 +1,11 @@
-*réalisation du moteur de jeu  
+*réalisation du moteur de jeu  Bastien
 
 
-* Représentation et changement des donjons
+* Représentation et changement des donjons Yannick
 
   
-* Interface graphique 
+* Interface graphique Yannick 
 
 
 
-*  
+*  sous taches à partager après
