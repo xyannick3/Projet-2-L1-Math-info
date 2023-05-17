d76fd4a9489a6620f343d15cc6b3bf502d766f94
xyannick3 <xyannick3@gmail.com>
2023-05-17 10:11:42 +0200

    push15052023

---
+++ changement/change.py
@@ -0,0 +1,60 @@
+
+class readin() :
+    def __init__(self) :
+        ...
+        self.map=[]
+    def translate(symbol) :
+        if symbol=="═" :
+            return (False,True,False,True)
+        if symbol=="║" :
+            return (True,False,True,False)
+        if symbol=="╔" :
+            return (False,True,True,False)
+        if symbol=="╗" :
+            return (False,False,True,True)
+        if symbol=="╚" :
+            return (True,True,False,False)
+        if symbol=="╝" :
+            return (True,False,False,True)
+        if symbol=="╠" :
+            return (True,True,True,False)
+        if symbol=="╣" :
+            return (True,False,True,True)
+        if symbol=="╦" :
+            return (False,True,True,True)
+        if symbol=="╩" :
+            return (True,True,False,True)
+        if symbol=="╨" :
+            return (True,False,False,False)
+        if symbol=="╡" :
+            return (False,False,False,True)
+        if symbol=="╥" :
+            return (False,False,True,False)
+        if symbol=="╞" :
+            return (False,True,False,False) 
+        if symbol=="╬" :
+            return (True,True,True,True)
+    def lire(filename) :
+        file = open(filename, encoding="utf8")
+        filebyline=file.readlines()
+        mazex=0
+        mazey=0
+        maze=[]
+        dragons=[]
+        for line in filebyline :
+            if line[0]=="A" :
+                # aventurier=(int(line[2]),int(line[4]))
+                aventurier={'position' : (int(line[2]),int(line[4])),'niveau':1}
+            if line[0]=="D" :
+                # dragons.append((int(line[2]),int(line[4]),int(line[6]))) Ancien système
+                dragons.append({'position' : (int(line[2]),int(line[4])),'niveau' :int(line[6])})
+            else : 
+                ligne=[]
+                for i in range(len(line)-1) : 
+                    ligne.append(readin.translate(line[i]))
+                if ligne[0]!=None :
+                    maze.append(ligne)
+        print(aventurier)
+        return maze, aventurier, dragons
+if __name__=="__main__" :
+    print(readin.lire("media/maps/map_test.txt"))
--- wallisyou.py
+++ wallisyou.py
@@ -1,3 +1,8 @@
+import graphisme.fltk as fltk
+import graphisme.graph as graph
+import moteur_de_jeu.engine as engine
+import changement.change as change
+
 #On ajoutera au fure et à mesure les imports et le code 
 #
 #
@@ -6,4 +11,49 @@
 #
 #
 if __name__=="__main__" :
-    ... # à remplir plus tard dans le projet
+    content=change.readin.lire("media/maps/map1.txt")
+    maze=content[0]
+    aventurier=content[1]
+    dragons=content[2]
+    resolution=(1000,1000)
+    test=graph.gameGraph()
+    test.CreateWindow()
+    test.displayMaze(maze)
+    test.knight(aventurier['position'][0],aventurier['position'][1],aventurier['niveau'])
+    for drag in dragons :
+        test.dragon(drag['position'][0],drag['position'][1],drag['niveau'])
+    print('donjon=',maze)
+    print('position = ',aventurier['position'])
+    print('dragons=',dragons)
+    while True :
+        print("intention",engine.intention(maze,aventurier['position'],dragons))
+        data=test.mappingclick(fltk.attend_clic_gauche())
+        data=[data[1],data[0]]
+        #print(data)
+        engine.pivoter(maze,data)
+        fltk.efface('wall')
+        fltk.efface('knight')
+        fltk.efface('dragon')
+        test.displayMaze(maze)
+        test.knight(aventurier['position'][0],aventurier['position'][1],aventurier['niveau'])
+        for drag in dragons :
+            test.dragon(drag['position'][0],drag['position'][1],drag['niveau'])
+    # fleche=[(0,0),(0,1),(1,1),(2,1),(2,2)]
+    # test.arrow(fleche)
+    # while True : 
+    #     data=test.mappingclick(fltk.attend_clic_gauche())
+    #     data=[data[1],data[0]]
+    #     print(data)
+    #     fltk.efface('knight')
+    #     fltk.efface('dragon')
+    #     fltk.efface('diamond')
+    #     engine.pivoter(listetest,data)
+    #     fltk.efface('wall')
+    #     test.displayMaze(listetest)
+    #     # modifier ce qu'il y a ci dessous pour tester les différentes images
+    #     # test.knight(data[0],data[1],1)
+    #     # test.dragon(data[0],data[1],2)
+    #     # test.diamond(data[0],data[1])
+
+
+    # ... # à remplir plus tard dans le projet
